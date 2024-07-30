85cbce8b137a0d91b72c1c9d734f94bc151eb1e7
pick-cee <akinloluwaolumuyide@gmail.com>
2024-07-30 13:06:07 +0100

    added cors

--- apps/cart/src/main.ts
+++ apps/cart/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(CartModule);
   const queueSvc = app.get<QueueService>(QueueService);
   const configSvc = app.get(ConfigService);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  })
   app.connectMicroservice(queueSvc.getOptions('product'));
   app.connectMicroservice(queueSvc.getOptions('cart'));
   app.connectMicroservice(queueSvc.getOptions('order'));
